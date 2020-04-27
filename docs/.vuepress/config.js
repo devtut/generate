aa4c1d2eea8e864e0efda2290fc8481199e47798
Imran <polboy777@gmail.com>
2020-04-27 13:50:09 +0600

    favicon related things added

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,57 @@
 module.exports = {
   title: "DevTut",
   description: "Lost Docs of Stackoverflow",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+    ],
+    ["link", { rel: "manifest", href: "/site.webmanifest" }],
+    ["meta", { name: "theme-color", content: "#ffffff" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "mask-icon",
+        href: "/safari-pinned-tab.svg",
+        color: "#5bbad5",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "msapplication-TileImage",
+        content: "/mstile-150x150.png",
+      },
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#da532c" }],
+  ],
   themeConfig: {
     repo: "devtut/generate",
     editLinks: true,
