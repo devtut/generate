9a352a181143f6d942b94b0e3a2e239c00b8856b
Imran Pollob <polboy777@gmail.com>
2026-05-31 16:20:47 -0400

    chore: add .nojekyll file and master branch initialization to deployment script

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,16 @@ yarn docs:build
 # navigate into the build output directory
 cd dist
 
+# bypass Jekyll processing in GitHub Pages to serve _astro/ assets
+touch .nojekyll
+
 echo 'User-agent: *
 Disallow:
 Sitemap: https://devtut.github.io/sitemap-index.xml
 Host: https://devtut.github.io' > robots.txt
 
 git init
+git checkout -b master
 git add -A
 git commit -m 'deploy'
 
